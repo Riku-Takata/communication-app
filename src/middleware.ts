7a9d969f8aa33ae20b2b370bbc177e8ea54600f2
Riku-Takata <riku_takata@icloud.com>
2024-12-11 23:43:45 +0900

    chdir middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,18 @@
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ['/', '/index', '/web-camera-app']
+  matcher: ['/', '/index', '/web-camera-app'],
 }
 
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
+
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
-    const [user, password] = atob(authValue).split(':')
+    const [user, pwd] = atob(authValue).split(':')
 
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      password === process.env.BASIC_AUTH_PASS
-    ) {
+    if (user === process.env.BASIC_ID && pwd === process.env.BASIC_PWD) {
       return NextResponse.next()
     }
   }
